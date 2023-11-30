40c9dd2497a1c1a49ea4504c13d9089eae942cff
Marsh4shoo <marshal.wjnr@gmail.com>
2023-11-30 12:17:55 -0800

    Updated

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Please provide a URL"
-    exit 1
-fi
-
-# Fetch and display the body of the response
+# a Bash script that displays the body of the response
 curl -s -L -X GET "$1"
