25a2df9f4844a13b759e8c03dd3aedfa36395dd4
Marsh4shoo <marshal.wjnr@gmail.com>
2023-12-01 11:09:19 -0800

    Updated

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-import urllib.parse
-import urllib.request
-import sys
+"""
+Python script that fetches an URL with requests package
+"""
+import requests
+
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-
-    req = urllib.request.Request(url, data=data, method='POST')
-
-    with urllib.request.urlopen(req) as response:
-        content = response.read().decode('utf-8')
-        print(content)
-
+    r = requests.get('https://intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
